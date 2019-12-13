11da213722481e326213f4aab81c07810c15b73f
jiahuanluo <luojiahuan001@gmail.com>
2019-12-13 16:07:09 +0800

    add train script

---
+++ train.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+GPU_ID=$1
+BATCH_SIZE=$2
+LR=$3
+WARMUP=$4
+EPOCH=$5
+DATASET=$6
+
+LOG_PATH="experiments/logs/${DATASET}/`date +'%Y-%m-%d_%H-%M-%S'`"
+if [ ! -d "$LOG_PATH" ]; then
+    mkdir -p "$LOG_PATH"
+fi
+LOG=${LOG_PATH}"/log.txt"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+set +x
+time CUDA_VISIBLE_DEVICES=${GPU_ID} python train.py \
+    -data_pkl data/lcsts/global_data/data.pkl \
+    -log ${LOG_PATH} \
+    -embs_share_weight \
+    -proj_share_weight \
+    -label_smoothing \
+    -save_model ${LOG_PATH} \
+    -epoch ${EPOCH} \
+    -b ${BATCH_SIZE} \
+    -warmup ${WARMUP} \
+    -lr ${LR} \
+    -save_mode all
