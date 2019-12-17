19b7229746a4c3d327b92f870331bfa4d6abf576
jiahuanluo <luojiahuan001@gmail.com>
2019-12-17 22:59:54 +0800

    translate

--- transformer/Translator.py
+++ transformer/Translator.py
@@ -12,7 +12,6 @@ class Translator(nn.Module):
     def __init__(
             self, model, beam_size, max_seq_len,
             src_pad_idx, trg_pad_idx, trg_bos_idx, trg_eos_idx):
-        
 
         super(Translator, self).__init__()
 
@@ -28,26 +27,24 @@ class Translator(nn.Module):
 
         self.register_buffer('init_seq', torch.LongTensor([[trg_bos_idx]]))
         self.register_buffer(
-            'blank_seqs', 
+            'blank_seqs',
             torch.full((beam_size, max_seq_len), trg_pad_idx, dtype=torch.long))
         self.blank_seqs[:, 0] = self.trg_bos_idx
         self.register_buffer(
-            'len_map', 
+            'len_map',
             torch.arange(1, max_seq_len + 1, dtype=torch.long).unsqueeze(0))
-
 
     def _model_decode(self, trg_seq, enc_output, src_mask):
         trg_mask = get_subsequent_mask(trg_seq)
         dec_output, *_ = self.model.decoder(trg_seq, trg_mask, enc_output, src_mask)
         return F.softmax(self.model.trg_word_prj(dec_output), dim=-1)
 
-
     def _get_init_state(self, src_seq, src_mask):
         beam_size = self.beam_size
 
         enc_output, *_ = self.model.encoder(src_seq, src_mask)
         dec_output = self._model_decode(self.init_seq, enc_output, src_mask)
-        
+
         best_k_probs, best_k_idx = dec_output[:, -1].topk(beam_size)
 
         scores = torch.log(best_k_probs.unsqueeze(0))
@@ -56,9 +53,8 @@ class Translator(nn.Module):
         enc_output = enc_output.repeat(beam_size, 1, 1)
         return enc_output, gen_seq, scores
 
-
     def _get_the_best_score_and_idx(self, gen_seq, dec_output, scores, step):
-        
+
         beam_size = self.beam_size
 
         # Get k candidates for each beam, k^2 candidates in total.
@@ -74,32 +70,31 @@ class Translator(nn.Module):
         gen_seq[:, step] = best_k_idx
 
         scores = best_k_probs.unsqueeze(0)
-        
-        return gen_seq, scores
 
+        return gen_seq, scores
 
     def translate_sentence(self, src_seq):
         # Only accept batch size equals to 1 in this function.
         # TODO: expand to batch operation.
         assert src_seq.size(0) == 1
 
-        src_pad_idx, trg_eos_idx = self.src_pad_idx, self.trg_eos_idx 
-        max_seq_len, beam_size, alpha = self.max_seq_len, self.beam_size, self.alpha 
+        src_pad_idx, trg_eos_idx = self.src_pad_idx, self.trg_eos_idx
+        max_seq_len, beam_size, alpha = self.max_seq_len, self.beam_size, self.alpha
 
         with torch.no_grad():
-            src_mask = get_pad_mask(src_seq, src_pad_idx)
+            src_mask = get_pad_mask(src_seq, src_pad_idx)  # (1, seq_len)
             enc_output, gen_seq, scores = self._get_init_state(src_seq, src_mask)
 
-            ans_idx = 0   # default
-            for step in range(2, max_seq_len):    # decode up to max length
+            ans_idx = 0  # default
+            for step in range(2, max_seq_len):  # decode up to max length
                 dec_output = self._model_decode(gen_seq[:, :step], enc_output, src_mask)
                 gen_seq, scores = self._get_the_best_score_and_idx(gen_seq, dec_output, scores, step)
 
                 # Check if all path finished
                 # -- locate the eos in the generated sequences
-                eos_locs = gen_seq == trg_eos_idx   
+                eos_locs = gen_seq == trg_eos_idx
                 # -- replace the eos with its position for the length penalty use
-                seq_lens, _ = self.len_map.masked_fill(1 - eos_locs, max_seq_len).min(1)
+                seq_lens, _ = self.len_map.masked_fill(~eos_locs, max_seq_len).min(1)
                 # -- check if all beams contain eos
                 if (eos_locs.sum(1) > 0).sum(0).item() == beam_size:
                     # TODO: Try different terminate conditions.
--- translate.py
+++ translate.py
@@ -9,10 +9,35 @@ import transformer.Constants as Constants
 from torchtext.data import Dataset
 from transformer.Models import Transformer
 from transformer.Translator import Translator
+from torch.utils.data import DataLoader
+
+import utils
+
+
+def prepare_mydataloaders(opt, device):
+    data = pickle.load(open(opt.data_pkl, 'rb'))
+
+    opt.max_token_seq_len = 140
+    opt.src_pad_idx = data['dict']['src'].labelToIdx[Constants.PAD_WORD]
+    opt.trg_pad_idx = data['dict']['tgt'].labelToIdx[Constants.PAD_WORD]
+    opt.trg_bos_idx = data['dict']['tgt'].labelToIdx[Constants.BOS_WORD]
+    opt.trg_eos_idx = data['dict']['tgt'].labelToIdx[Constants.EOS_WORD]
+    opt.unk_idx = 1
+    opt.src_vocab_size = len(data['dict']['src'].labelToIdx)
+    opt.trg_vocab_size = len(data['dict']['tgt'].labelToIdx)
+    # ========= Preparing Model =========#
+    # if opt.embs_share_weight:
+    #     assert data['dict']['src'].labelToIdx == data['dict']['tgt'].labelToIdx, \
+    #         'To sharing word embedding the src/trg word2idx table shall be the same.'
+    testset = utils.BiDataset(data['test'])
+    testloader = torch.utils.data.DataLoader(dataset=testset,
+                                             batch_size=1,
+                                             shuffle=False,
+                                             collate_fn=utils.padding)
+    return data['dict']['tgt'], testloader
 
 
 def load_model(opt, device):
-
     checkpoint = torch.load(opt.model, map_location=device)
     model_opt = checkpoint['settings']
 
@@ -36,7 +61,7 @@ def load_model(opt, device):
 
     model.load_state_dict(checkpoint['model'])
     print('[Info] Trained model state loaded.')
-    return model 
+    return model
 
 
 def main():
@@ -56,30 +81,30 @@ def main():
     parser.add_argument('-no_cuda', action='store_true')
 
     # TODO: Translate bpe encoded files 
-    #parser.add_argument('-src', required=True,
+    # parser.add_argument('-src', required=True,
     #                    help='Source sequence to decode (one line per sequence)')
-    #parser.add_argument('-vocab', required=True,
+    # parser.add_argument('-vocab', required=True,
     #                    help='Source sequence to decode (one line per sequence)')
     # TODO: Batch translation
-    #parser.add_argument('-batch_size', type=int, default=30,
+    # parser.add_argument('-batch_size', type=int, default=30,
     #                    help='Batch size')
-    #parser.add_argument('-n_best', type=int, default=1,
+    # parser.add_argument('-n_best', type=int, default=1,
     #                    help="""If verbose is set, will output the n_best
     #                    decoded sentences""")
 
     opt = parser.parse_args()
     opt.cuda = not opt.no_cuda
 
-    data = pickle.load(open(opt.data_pkl, 'rb'))
-    SRC, TRG = data['vocab']['src'], data['vocab']['trg']
-    opt.src_pad_idx = SRC.vocab.stoi[Constants.PAD_WORD]
-    opt.trg_pad_idx = TRG.vocab.stoi[Constants.PAD_WORD]
-    opt.trg_bos_idx = TRG.vocab.stoi[Constants.BOS_WORD]
-    opt.trg_eos_idx = TRG.vocab.stoi[Constants.EOS_WORD]
+    # data = pickle.load(open(opt.data_pkl, 'rb'))
+    # SRC, TRG = data['vocab']['src'], data['vocab']['trg']
+    # opt.src_pad_idx = SRC.vocab.stoi[Constants.PAD_WORD]
+    # opt.trg_pad_idx = TRG.vocab.stoi[Constants.PAD_WORD]
+    # opt.trg_bos_idx = TRG.vocab.stoi[Constants.BOS_WORD]
+    # opt.trg_eos_idx = TRG.vocab.stoi[Constants.EOS_WORD]
 
-    test_loader = Dataset(examples=data['test'], fields={'src': SRC, 'trg': TRG})
-    
+    # test_loader = Dataset(examples=data['test'], fields={'src': SRC, 'trg': TRG})
     device = torch.device('cuda' if opt.cuda else 'cpu')
+    TRG, test_loader = prepare_mydataloaders(opt, device)
     translator = Translator(
         model=load_model(opt, device),
         beam_size=opt.beam_size,
@@ -89,18 +114,16 @@ def main():
         trg_bos_idx=opt.trg_bos_idx,
         trg_eos_idx=opt.trg_eos_idx).to(device)
 
-    unk_idx = SRC.vocab.stoi[SRC.unk_token]
     with open(opt.output, 'w') as f:
         for example in tqdm(test_loader, mininterval=2, desc='  - (Test)', leave=False):
-            #print(' '.join(example.src))
-            src_seq = [SRC.vocab.stoi.get(word, unk_idx) for word in example.src]
-            pred_seq = translator.translate_sentence(torch.LongTensor([src_seq]).to(device))
-            pred_line = ' '.join(TRG.vocab.itos[idx] for idx in pred_seq)
+            sec_seq = example[0].view(1, -1)
+            pred_seq = translator.translate_sentence(sec_seq.to(device))
+            pred_line = ' '.join(TRG.idxToLabel[idx] for idx in pred_seq)
             pred_line = pred_line.replace(Constants.BOS_WORD, '').replace(Constants.EOS_WORD, '')
-            #print(pred_line)
-            f.write(pred_line + '\n')
+            f.write(pred_line)
 
     print('[Info] Finished.')
+
 
 if __name__ == "__main__":
     '''
